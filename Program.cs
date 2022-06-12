997517380e810fa8a77dfa864af29c38a17639b3
AngeloStatescu <35111313+AngeloStatescu@users.noreply.github.com>
2022-06-13 02:16:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+{
+    var services = builder.Services;
+    services.AddControllers();
+}
+
+var app = builder.Build();
+
+{
+    app.MapControllers();
+}
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,4 @@
+###Web Trade
+#Mock API
+
+todo...
